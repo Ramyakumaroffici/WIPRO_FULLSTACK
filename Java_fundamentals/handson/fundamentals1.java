04c4fc364b1cbf3a988f2aff07b9b9db6b46abc7
Ramyakumaroffici <ramyakumaroffici@gmail.com>
2025-07-18 11:43:49 +0530

    ready

---
+++ Java_fundamentals/handson/fundamentals1.java
@@ -0,0 +1,12 @@
+public class fundamentals1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String firstargu=args[0];
+		String secondargu=args[1];
+		System.out.println(firstargu+" "+"Technologies"+" " +secondargu);
+
+	}
+
+}
+
